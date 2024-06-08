5f3d8c01d165d44950379d35f0c76e73a4b0b0f6
777hanh777hanh <thanhthientran151100@gmail.com>
2024-06-08 23:14:29 +0700

    fix: change url of image for meta tag

--- src/utils/renderMetaTag.tsx
+++ src/utils/renderMetaTag.tsx
@@ -1,7 +1,6 @@
 import { links } from '~/routes';
-import { staticImages } from '~/assets/images';
 
-const image = staticImages.ogImage;
+const image = 'https://www.tranthienthanh.online/assets/meta-img-C_VNZq7i.png';
 const title = 'Personal Portfolio | Tran Thien Thanh';
 const desc = 'This project is my portfolio which features some of my skills, GitHub projects, and my resume.';
 const keywords = 'portfolio, Tran Thien Thanh, React, HTML, CSS, SCSS, TypeScript, JavaScript';
