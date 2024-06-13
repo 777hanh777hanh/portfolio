82a766758cf5dca96bc132daec11d2f016b03386
777hanh777hanh <thanhthientran151100@gmail.com>
2024-06-13 22:57:39 +0700

    build(vite.config): update basement in production eviroment

--- vite.config.ts
+++ vite.config.ts
@@ -23,5 +23,5 @@ export default defineConfig({
         // Set port to listen on 3000
         port: 3000,
     },
-    base: process.env.NODE_ENV === 'development' ? './' : '/portfolio',
+    base: process.env.NODE_ENV === 'development' ? './' : '/',
 });
